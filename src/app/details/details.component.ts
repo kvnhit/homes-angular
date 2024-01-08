8b8f4f9f381901dc345cb1f06924e5df60fe81ee
kvnhit <kevinhitzschky@outlook.com>
2024-01-08 17:36:16 -0300

    Adicionando Formulário e tela de details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -4,11 +4,12 @@ import { ActivatedRoute } from '@angular/router'
 import { HousingService } from '../housing.service'
 import { HousingLocation } from '../housinglocation'
 import { HousingLocationComponent } from '../housing-location/housing-location.component'
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
 	selector: 'app-details',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, ReactiveFormsModule],
 	template: `
 		<article>
 			<img class="listing-photo" [src]="housingLocation?.photo" />
@@ -26,7 +27,17 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
 			</section>
 			<section class="listing-apply">
 				<h2 class="section-heading">Apply now to live here!</h2>
-				<button class="primary" type="button">Apply now</button>
+				<form [formGroup]="applyForm" (submit)="submitApplication()">
+					<label for="first-name">First Name</label>
+					<input id="first-name" type="text" formControlName="firstName" />
+
+					<label for="last-name">Last Name</label>
+					<input id="last-name" type="text" formControlName="lastName" />
+
+					<label for="email">Email</label>
+					<input id="email" type="text" formControlName="email" />
+					<button type="submit" class="primary">Apply now</button>
+				</form>
 			</section>
 		</article>
 	`,
@@ -36,9 +47,18 @@ export class DetailsComponent {
 	route: ActivatedRoute = inject(ActivatedRoute)
 	housingService = inject(HousingService)
 	housingLocation: HousingLocation | undefined
+	applyForm = new FormGroup({
+		firstName: new FormControl(''),
+		lastName: new FormControl(''),
+		email: new FormControl(''),
+	})
 
 	constructor() {
 		const housingLocationId = Number(this.route.snapshot.params['id'])
 		this.housingLocation = this.housingService.getHousingLocationById(housingLocationId)
+	}
+
+	submitApplication() {
+		this.housingService.submitApplication(this.applyForm.value.firstName ?? '', this.applyForm.value.lastName ?? '', this.applyForm.value.email ?? '')
 	}
 }
